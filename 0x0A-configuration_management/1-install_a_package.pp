e162736c5d4c8f8ea2f243185f4c2d8df81c20c2
MairaQuillQuest <ririsoniah@gmail.com>
2024-03-22 14:28:09 -0700

    [Feature]Adding Configuration management project

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => "/usr/bin/pip3 freeze | grep Flask==2.1.0",
-  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+# Install flask using puppet
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }

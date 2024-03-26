e93e55ecdf331d75ed8bbc9b4e6e9e85c8434908
MairaQuillQuest <ririsoniah@gmail.com>
2024-03-26 09:26:22 -0700

    specify the exact version of Werkzeug that is compatible with Flask 2.1.0.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-# Install flask using puppet
-
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1', # or the version that is compatible with Flask 2.1.0
   provider => 'pip3'
 }

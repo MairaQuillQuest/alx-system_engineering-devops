b810405f72f13a1a1ccc0ff39e063c8949c437bd
MairaQuillQuest <ririsoniah@gmail.com>
2024-05-08 00:53:17 -0700

    checks if the status code is 200 (indicating a successful request) and if the data field exists in the response JSON and contains the subscribers field.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,18 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    data = response.json().get("data", None)
+    if data is None or "subscribers" not in data:
+        return 0
+    return data["subscribers"]
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))

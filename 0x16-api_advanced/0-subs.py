ddc37a394d626b6baa5cc637fb17ec6840d9304f
MairaQuillQuest <ririsoniah@gmail.com>
2024-05-07 18:17:40 -0700

    [Feature]:adding API advanced project

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-import urllib.parse
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(urllib.parse.quote(subreddit.lower()))
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "MyBot/0.1"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    if response.is_redirect:
-        return 0  # Handle redirects
     results = response.json().get("data")
     return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
-import urllib.parse
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(urllib.parse.quote(subreddit.lower()))
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "MyBot/0.1"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
-        return 0
-    if response.is_redirect:
-        return 0  # Handle redirects
+        print("None")
+        return
     results = response.json().get("data")
-    return results.get("subscribers")
+    [print(c.get("data").get("title")) for c in results.get("children")]
